ab31bfa02358a1c776a873fdc929b7c9f94e9d86
obada.kahlous <obada.kahlous@nextinuous.com>
2025-05-08 17:07:02 +0300

    Add pages for mobile and accessories

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -17,6 +17,22 @@ const Navbar = () => {
 
   return (
     <nav className="sticky top-0 left-0 w-full h-[70px] bg-white overflow-hidden shadow-md z-50">
+      <ul className="flex items-center justify-center ml-[40px]">
+        {navLinks.map((link) => {
+          const isActive = pathname?.startsWith(link.href)
+          return (
+            <Link
+              href={link.href}
+              key={link.name}
+              className={`${isActive ? "active-link" : "link"}`}
+            >
+              <li className="text-[#fff] text-[1rem] font-[400]">
+                {link.name}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
       <div className="container mx-auto flex justify-between items-center h-[70px]">
         <Link href={"/"} className="flex items-center gap-2">
           <h4 className="text-[22px] font-[700] md:text-left text-center font-sans">
@@ -24,23 +40,6 @@ const Navbar = () => {
           </h4>
           <img src="/logo.png" alt="icon" width={"60px"} />
         </Link>
-        <ul className="flex items-center justify-center ml-[40px]">
-          {navLinks.map((link) => {
-            const isActive = pathname?.startsWith(link.href);
-
-            return (
-              <Link
-                href={link.href}
-                key={link.name}
-                className={`${isActive ? "active-link" : "link"}`}
-              >
-                <li className="text-white text-[1rem] font-[400]">
-                  {link.name}
-                </li>
-              </Link>
-            );
-          })}
-        </ul>
       </div>
     </nav>
   );
